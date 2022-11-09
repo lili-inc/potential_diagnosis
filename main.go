af156ab0fe3cb7086c90e64a8bd65f347ffd4d4c
Tatsuya Ichikawa <oral.dr31@gmail.com>
2022-11-09 11:14:05 +0900

    add yaml

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"lili_style_test/src"
 	"log"
 	"net/http"
 )
 
-//func main() {
-//	router := src.GetRouter()
-//	router.Run(":8080")
-//}
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World!")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := src.GetRouter()
+	router.Run(":8080")
 }
+
+//func main() {
+//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//		fmt.Fprintln(w, "Hello World!")
+//	})
+//	log.Fatal(http.ListenAndServe(":8080", nil))
+//}
